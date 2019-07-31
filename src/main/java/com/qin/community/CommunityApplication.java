2bc3184bd98b9c9fc33082ec96ca0ad4e30ed5ba
jack <15777468232@163.com>
2019-07-31 15:30:41 +0800

    init repo

---
+++ src/main/java/com/qin/community/CommunityApplication.java
@@ -0,0 +1,20 @@
+package com.qin.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* @Description:
+* @Param:
+* @return:
+* @Author: Qin
+* @Date: 2019/7/31
+*/
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
